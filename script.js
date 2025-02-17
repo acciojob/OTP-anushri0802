640c6d5583dd3c68c16310898a1cfa11305934f2
Anushri <112797477+anushri0802@users.noreply.github.com>
2025-02-17 20:52:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', (event) => {
+    const inputs = document.querySelectorAll('.code-box');
+    const firstInput = document.getElementById('code-1');
+    
+    // Set focus on the first input field
+    firstInput.focus();
+    
+    inputs.forEach((input, index) => {
+        input.addEventListener('input', (event) => {
+            if (event.target.value.length > 0 && index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
+        });
+
+        input.addEventListener('keydown', (event) => {
+            if (event.key === 'Backspace') {
+                if (event.target.value.length === 0 && index > 0) {
+                    inputs[index - 1].focus();
+                }
+            }
+        });
+    });
+});
